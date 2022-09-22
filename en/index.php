40d4ffa614542b999d9f0092fe4196559b20b1b7
badassboy <emmanuelobeng7788@gmail.com>
2022-09-22 07:58:33 +0100

    index file

--- en/index.php
+++ en/index.php
@@ -80,20 +80,18 @@ include_once 'inc/carter.php';
     function saveIP($user_ip)
     {
 
-<<<<<<< HEAD
         // $db_host = "localhost";
         // $db_user = "root";
         // $db_password = "";
         // $db_name= "minazy";
 
         include_once "db.php";
-=======
+
        
-        $db_host = "localhost";
-        $db_user = "root";
-        $db_password = "";
-        $db_name= "minazy";
->>>>>>> 8ecc8a7abed2a2f6d7c49348d5082958594cd913
+        // $db_host = "localhost";
+        // $db_user = "root";
+        // $db_password = "";
+        // $db_name= "minazy";
 
         // create connection
         $conn = mysqli_connect($db_host,$db_user,$db_password,$db_name);
